3bc23c8a99963496942c60e08b37980868ae2e61
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-08-20 16:10:50 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
 from models import Dog
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+Base = declarative_base()
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+    engine = create_engine('sqlite:///dogs.db:')
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///dogs.db:')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session_instance = Session()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
-
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
